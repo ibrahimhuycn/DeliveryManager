80a735901e09beb1df1d4203fba7479152ce37f7
Ibrahim Hussain <ibrahim.hussain@medtechmaldives.com>
2019-06-22 21:12:36 +0500

    made model properties public

--- DeliveryManaer/Models/DeliveryLogsModel.vb
+++ DeliveryManaer/Models/DeliveryLogsModel.vb
@@ -1,11 +1,11 @@
 ﻿Public Class DeliveryLogsModel
-    Property DeliveryNo As String
-    Property Customer As String
-    Property CustomerPoNumber As String
-    Property Godown As String
-    Property DeliveredBy As String
-    Property DeliveredDate As Date
-    Property ReceivedBy As String
-    Property ReceivedDate As Date
-    Property Comment As String
+    Public Property DeliveryNo As String
+    Public Property Customer As String
+    Public Property CustomerPoNumber As String
+    Public Property Godown As String
+    Public Property DeliveredBy As String
+    Public Property DeliveredDate As Date
+    Public Property ReceivedBy As String
+    Public Property ReceivedDate As Date
+    Public Property Comment As String
 End Class
