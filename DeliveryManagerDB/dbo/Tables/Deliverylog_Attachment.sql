b5d4927bd1197d732623a3d710ffc98cefd62bbf
Ibrahim Hussain <ibrahim.hussain@medtechmaldives.com>
2019-06-24 17:31:33 +0500

    Added foriegn keys to DeliveryLog_Attachment table

--- DeliveryManagerDB/dbo/Tables/Deliverylog_Attachment.sql
+++ DeliveryManagerDB/dbo/Tables/Deliverylog_Attachment.sql
@@ -3,5 +3,8 @@
 	[Id] INT NOT NULL PRIMARY KEY, 
     [DeliveryNumber] VARCHAR(20) NOT NULL, 
     [AttachmentId] INT NOT NULL, 
-    [AttachmentType] INT NOT NULL
+    [AttachmentTypeId] INT NOT NULL, 
+    CONSTRAINT [FK_DeliveryLog_Attachment_ToDeliveryLog] FOREIGN KEY ([DeliveryNumber]) REFERENCES [DeliveryLog]([DeliveryNumber]), 
+    CONSTRAINT [FK_DeliveryLog_Attachment_ToAttachmentId] FOREIGN KEY ([AttachmentId]) REFERENCES [Attachment]([Id]), 
+    CONSTRAINT [FK_DeliveryLog_Attachment_ToAttachmentType] FOREIGN KEY ([AttachmentTypeId]) REFERENCES [AttachmentType]([Id])
 )
