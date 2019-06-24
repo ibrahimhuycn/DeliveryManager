283d5ec60d35775cd5f1a6ca19f9c0c2dc1f7fbd
Ibrahim Hussain <ibrahim.hussain@medtechmaldives.com>
2019-06-24 17:35:25 +0500

    Added foriegn keys to DeliveryTraceLog

--- DeliveryManagerDB/dbo/Tables/DeliveryTraceLog.sql
+++ DeliveryManagerDB/dbo/Tables/DeliveryTraceLog.sql
@@ -1,8 +1,11 @@
 ﻿CREATE TABLE [dbo].[DeliveryTraceLog]
 (
 	[Id] INT NOT NULL PRIMARY KEY,
-	[DeliveryLogNUmber] VARCHAR(20) NOT NULL,
+	[DeliveryNumber] VARCHAR(20) NOT NULL,
     [TraceTypeId] INT NOT NULL, 
     [EmployeeId] INT NOT NULL, 
-    [LoggedTime] DATETIME NOT NULL
+    [LoggedTime] DATETIME NOT NULL, 
+    CONSTRAINT [FK_DeliveryTraceLog_ToDeliveryLog] FOREIGN KEY ([DeliveryNumber]) REFERENCES [DeliveryLog]([DeliveryNumber]), 
+    CONSTRAINT [FK_DeliveryTraceLog_ToTraceType] FOREIGN KEY ([TraceTypeId]) REFERENCES [TraceType]([Id]), 
+    CONSTRAINT [FK_DeliveryTraceLog_ToEmployee] FOREIGN KEY ([EmployeeId]) REFERENCES [Employee]([Id])
 )
